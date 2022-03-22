3124191149eb29b95924a831b32ed49f0fd9e50d
rxtthin <rotthin_dev@protonmail.com>
2022-03-22 15:02:03 +0100

    fixed untracked files

--- include/rgl_file.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RGL_FILE_H
-#define __RGL_FILE_H
-
-#include "rgl_common.h"
-
-char *rglReadFile(const char *path);
-
-#endif /* __RGL_FILE_H */
--- include/rgl_log.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RGL_LOG_H
-#define __RGL_LOG_H
-
-#define RGL_LOG(fmt, args...) printf("[RGL] " fmt "\n", ##args)
-#define RGL_LOG_ERROR(fmt, args...) fprintf(stderr, "[RGL ERROR] " fmt "\n", ##args)
-
-#endif /* __RGL_LOG_H */
