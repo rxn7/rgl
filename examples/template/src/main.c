34ecf166e9a177f633fd4a44db188754fc3b82bc
rxtthin <rotthin_dev@protonmail.com>
2022-04-05 14:43:16 +0200

    Randomize colors on key press

--- examples/template/src/main.c
+++ examples/template/src/main.c
@@ -4,6 +4,9 @@ void appInit(void);
 void appUpdate(f32 dt);
 void appQuit(void);
 void appDraw(void);
+void randomizeColors(void);
+
+rglColor colors[4] = {};
 
 int
 main(int argc, const char **argv) {
@@ -24,22 +27,34 @@ main(int argc, const char **argv) {
 void
 appInit(void) {
 	puts("RGL has initialised.");
+	randomizeColors();
 }
 
 void
 appUpdate(f32 dt) {
 	printf("FPS: %f\n", 1.f / dt);
+
+	if(rglIsKeyJustPressed(RGL_KEY_SPACE)) {
+		randomizeColors();
+	}
 }
 
 void
 appDraw(void) {
-	rglDrawCircleOutline(RGL_RED, (rglV2){-450,0}, 100, 4);
-	rglDrawCircle(RGL_RGB(255,255,0), (rglV2){-200,0}, 100);
-	rglDrawRect(RGL_RGB(255,150,0), (rglV2){200,0}, (rglV2){100,100});
-	rglDrawRectOutline(RGL_GREEN, (rglV2){450,0}, (rglV2){100,100}, 4);
+	rglDrawCircleOutline(colors[0], (rglV2){-450,0}, 100, 4);
+	rglDrawCircle(colors[1], (rglV2){-200,0}, 100);
+	rglDrawRect(colors[2], (rglV2){200,0}, (rglV2){100,100});
+	rglDrawRectOutline(colors[3], (rglV2){450,0}, (rglV2){100,100}, 4);
 }
 
 void
 appQuit(void) {
 	puts("Quitting...");
+}
+
+void
+randomizeColors(void) {
+	for(u8 i=0; i<4; ++i) {
+		colors[i] = RGL_COLOR_RAND;
+	}
 }
