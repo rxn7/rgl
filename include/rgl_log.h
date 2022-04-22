ad7802d2b6602095ffb6de63d393d346f2d01e65
Rotthin <rotthin_dev@protonmail.com>
2022-04-22 04:58:10 +0200

    Colored logs

--- include/rgl_log.h
+++ include/rgl_log.h
@@ -1,7 +1,16 @@
 #ifndef __RGL_LOG_H
 #define __RGL_LOG_H
 
-#define RGL_LOG(fmt, args...) printf("[RGL] " fmt "\n", ##args)
-#define RGL_LOG_ERROR(fmt, args...) fprintf(stderr, "[RGL ERROR] " fmt "\n", ##args)
+#define ANSII_WHITE "\e[0;37m"
+#define ANSII_RED "\e[0;31m"
+#define ANSII_YELLOW "\e[0;33m"
+
+#define ANSII_BOLD_WHITE "\e[1;37m"
+#define ANSII_BOLD_RED "\e[1;31m"
+#define ANSII_BOLD_YELLOW "\e[1;33m"
+
+#define RGL_LOG(fmt, args...) printf(ANSII_BOLD_WHITE "[RGL] " fmt "\n" ANSII_WHITE, ##args)
+#define RGL_LOG_ERROR(fmt, args...) printf(ANSII_BOLD_RED "[RGL ERROR] " fmt "\n" ANSII_WHITE, ##args)
+#define RGL_LOG_WARN(fmt, args...) print(ANSII_BOLD_YELLOW "[RGL WARN] " fmt "\n" ANSII_WHITE, ##args)
 
 #endif /* __RGL_LOG_H */
